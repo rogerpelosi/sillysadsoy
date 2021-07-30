adfd6ba50280d16aeaa97828f9173fa42ec6d938
roger chat <mattrogerpelosi@gmail.com>
2021-07-30 14:44:56 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,9 @@
+const h2 = document.createElement('h2');
+h2.textContent = 'This content has been added using JavaScript!';
+
+document.querySelector('body').appendChild(h2);
+
+const h3 = document.createElement('h3');
+h3.textContent = 'this was as well :-)';
+
+document.querySelector('body').appendChild(h3);
